83d5b2be34be85c2daa796fb20eb2292f01cbddc
Luisesh1 <luisesh2@hotmail.com>
2016-01-10 19:09:42 -0700

    filtro usuraio

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,11 @@ class WelcomeController < ApplicationController
   	@cont = Victim.count()
   end
   def victimas
-    @vict= Victim.where(:swusu => current_user)
+    require 'pg'
+    conn = PGconn.open(:dbname => 'production')
+    @vict = conn.exec('SELECT usu , con from victims')
+
+    #@vict= Victim.where(:swusu => current_user)
   end
   def ataques
   end
