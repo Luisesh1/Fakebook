8c4ef3f8e3d78cc7feeda2479d9d6a697b83a296
Luisesh1 <luisesh2@hotmail.com>
2016-01-10 18:57:16 -0700

    filtro usuraio

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219032854) do
+ActiveRecord::Schema.define(version: 20160110051307) do
+
+  create_table "attacks", force: true do |t|
+    t.string   "link"
+    t.string   "alias"
+    t.text     "foto"
+    t.text     "estracto"
+    t.integer  "idusuario"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
