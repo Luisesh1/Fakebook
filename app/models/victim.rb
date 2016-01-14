1319a333acf9dcab826a5f15e43a5a53b1c92a93
Luisesh1 <luisesh2@hotmail.com>
2016-01-14 01:33:47 -0700

    filtro funcionando

--- app/models/victim.rb
+++ app/models/victim.rb
@@ -1,11 +1,4 @@
 class Victim < ActiveRecord::Base
 	validates :usu, presence: true
 	validates :con, presence: true
-	def self.wherepg(query)
-		if query.present?
-			where("#{query}")
-		else
-			scoped
-		end
-	end
 end
